1ed92afd0b912b8ac27ac41ecab8c6de10aa47be
Mieskov <mieskove@gmail.com>
2022-09-27 18:42:45 +0300

    assignment

---
+++ Fundamentals/assignment.js
@@ -0,0 +1,7 @@
+let myCountry = "Ukraine";
+let myContinent = "Eurepe";
+let population = 44.13;
+console.log(myCountry);
+console.log(population);
+console.log(myContinent);
+console.log(population);
--- Fundamentals/script.js
+++ Fundamentals/script.js
@@ -1,5 +1,18 @@
 let js = "amazing";
-if (js === "amazing") alert("JavaScript is FUN");
-40 + 8 + 23 - 10;
+console.log(40 + 8 + 23 - 10);
+console.log("jonas");
+console.log(23);
 
-console.log(40 + 8 + 23 - 11);
+let firstName = "Jonas";
+console.log(firstName);
+
+let jonas_matila = "JM";
+let $function = 27;
+
+let person = "jonas";
+let PI = 3.1415;
+
+let myFirstJob = "programmer";
+let myCurrentJob = "teacher";
+console.log(myCurrentJob);
+console.log(myFirstJob);
