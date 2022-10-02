b1195dbdff296015f07cee05b09ae01a5030f0b1
Mieskov <mieskove@gmail.com>
2022-10-02 12:43:46 +0300

    height

--- Fundamentals/script.js
+++ Fundamentals/script.js
@@ -159,9 +159,24 @@ n = n - 1; // 10 number
 console.log(n);
 
 */
+// 5 folsy values: 0, '' ,undefined, null, NaN
+
 const money = 100;
 if (money) {
   console.log("Don't spend it all ;)");
 } else {
   console.log("You should get a job!");
 }
+
+let height;
+if (height) {
+  console.log("YAY! Height is defined");
+} else {
+  console.log("Height is UNDEFINED");
+}
+
+const favourite = Number(prompt("What's your favourite number?"));
+console.log(favourite);
+console.log(typeof favourite);
+
+if (favourite === 23) console.log("Cool!! 23 is an amzaing number!");
