655e8514e68dc7137b85f283446cc1b2b1f59b96
Mieskov <mieskove@gmail.com>
2022-10-06 17:46:10 +0300

    drive

--- Fundamentals/script.js
+++ Fundamentals/script.js
@@ -188,3 +188,18 @@ else {
 
 if (favourite !== 23) console.log("Why not 23?");
 */
+
+const hasDriverLicense = true;
+const hasGoodVision = true;
+
+console.log(hasDriverLicense && hasGoodVision);
+console.log(hasDriverLicense || hasGoodVision);
+console.log(!hasDriverLicense);
+
+const isTired = true;
+console.log(hasDriverLicense && hasGoodVision && isTired);
+if (hasDriverLicense && hasGoodVision && !isTired) {
+  console.log("Sarah is able to drive!");
+} else {
+  console.log("Someone else should drive...");
+}
