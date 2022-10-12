5e9f822cf6a697898cd8e4061e9e929a2ea24518
Mieskov <mieskove@gmail.com>
2022-10-12 20:13:27 +0300

    juice

--- script.js
+++ script.js
@@ -4,3 +4,13 @@ function logger() {
   console.log("My name is Jack");
 }
 logger();
+
+function fruitProcessor(appels, oranges) {
+  const juice = `Juice with ${appels} appels and ${oranges} oranges.`;
+  return juice;
+}
+const appeleJuice = fruitProcessor(5, 0);
+console.log(appeleJuice);
+
+const appleOrange = fruitProcessor(2, 4);
+console.log(appleOrange);
