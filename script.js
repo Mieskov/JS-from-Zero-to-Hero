b09112bc493708307f211e2eeb881dcb71608c94
Mieskov <mieskove@gmail.com>
2022-10-12 20:21:38 +0300

    car

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function logger() {
 }
 logger();
 
-function fruitProcessor(appels, oranges) {
+/*function fruitProcessor(appels, oranges) {
   const juice = `Juice with ${appels} appels and ${oranges} oranges.`;
   return juice;
 }
@@ -14,3 +14,11 @@ console.log(appeleJuice);
 
 const appleOrange = fruitProcessor(2, 4);
 console.log(appleOrange);
+*/
+
+function carRent(BMW, audi) {
+  const car = `How many ${BMW} BMW and ${audi} audi do you have?`;
+  return car;
+}
+const carNum = carRent(2, 5);
+console.log(carNum);
