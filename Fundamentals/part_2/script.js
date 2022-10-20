cb1ba957c4d5a870a5ddd3c52b645875c6e81329
Mieskov <mieskove@gmail.com>
2022-10-20 12:10:54 +0300

    obgect

--- Fundamentals/part_2/script.js
+++ Fundamentals/part_2/script.js
@@ -14,7 +14,7 @@ console.log(appeleJuice);
 
 const appleOrange = fruitProcessor(2, 4);
 console.log(appleOrange);
-*/
+*/ /*
 /*
 // Function declaration
 function calcAge1(birthYear) {
@@ -205,17 +205,19 @@ if (friends.includes("Jack")) {
   console.log("Jack is my friend!");
 }
 */
+
+/*
 const calcTip = function (bill) {
   return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
 };
 
-/*const calcTip = function (bill) {
+const calcTip = function (bill) {
   if (bill <= 300 && bill >= 50) {
     return bill * 0.15;
   } else {
     return bill * 0.2;
   }
-};*/
+};
 
 console.log(calcTip(50));
 
@@ -224,3 +226,4 @@ const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
 console.log(tips);
 const total = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
 console.log(total);
+*/
